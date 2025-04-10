4af7ca6077b5a1aa0c193c7b3dd3fa856f12c3be
Damir Magdić <damir.magdic@vsite.hr>
2025-04-10 15:49:31 +0200

    polyline + escape&backspace

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
+#include <algorithm>
 
 class main_window : public vsite::nwp::window
 {
+private:
+	std::list<POINT> points;
 protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
+	void on_paint(HDC hdc) override
+	{
+		if (points.empty()) return; // nothing to draw
+		else {
+			MoveToEx(hdc, points.front().x, points.front().y, NULL);
+			for (POINT p : points)
+			{
+				LineTo(hdc, p.x, p.y); // draw line to each point
+			}
+			// TODO: iterate over points in container and draw polyline
+		}
 	}
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+		points.push_back(p); // add point to container
+		InvalidateRect(*this, nullptr, true); // request repaint
 	}
+	// TODO: add point to container
+	
 	void on_key_down(int vk) override
 	{
+		switch (vk)
+		{
+		case VK_ESCAPE:
+		{
+			points.clear(); // clear container
+			InvalidateRect(*this, nullptr, true); // request repaint
+			break;
+		}
+		case VK_BACK:
+		{
+			if (!points.empty())
+			{
+				points.pop_back(); // remove last point
+				InvalidateRect(*this, nullptr, true); // request repaint
+			}
+			break;
+		}
+		default:
+			break;
+		}
+
 	// TODO: Esc - empty container
 	// TODO: Backspace - remove last point
 	}
